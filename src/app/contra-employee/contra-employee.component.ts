f4c5d5bea08ea56dc829edaecf2c7a0e90e246d2
Ragav9 <130131236+Ragav9@users.noreply.github.com>
2023-04-19 18:36:38 +0530

    contractorEmployee TS added

--- src/app/contra-employee/contra-employee.component.ts
+++ src/app/contra-employee/contra-employee.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contra-employee',
@@ -7,6 +8,32 @@ import { Component } from '@angular/core';
 })
 export class ContraEmployeeComponent {
 
-  
-
+  constructor(private fb:FormBuilder){
+    
+  }
+  ContractEmployeeForm = this.fb.group({
+    contractorName:[''],
+    employeeName:[''],
+    spouseName:[''],
+    maritalStatus:[''],
+    DOB:[''],
+    EmpMobileNo:['', Validators.required],
+    gender:[''],
+    adhaarNo:[''],
+    address:[''],
+    pincode:[''],
+    city:[''],
+    state:[''],
+    emergencyContactNo:[''],
+    emergencyContactPerson:[''],
+    emergencyContactRelation:[''],
+    bloodGroup:[''],
+    PF_UAN:[''],
+    ESI_No:[''],
+    transporter:[''],
+    village:[''],
+  })
+  onSubmit() {
+    console.log(this.ContractEmployeeForm.value);
+  }
 }
